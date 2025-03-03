0579459e84a21588cbe1609e3c458f08b55f7be2
Saboktakin Rizi, Bahareh (B.) <bsabokta@ford.com>
2025-03-03 11:04:09 +0000

    feat: ignore types for now

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+import Overton from "./Overton.jsx";
+function App() {
+  return <Overton />;
+}
+
+export default App;
---
+++ src/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module "*.jsx" {
+  import React from "react";
+  const JSXComponent: React.ComponentType<any>;
+  export default JSXComponent;
+}
